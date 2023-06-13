93e17874bb5719f8ebb02f73c4311c142b65fb1b
Partho Roy <roypartho772@gmail.com>
2023-06-14 01:58:35 +0600

    jwt token verify added

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized Access" });
+  }
+  const token = authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).send("Unauthorized Access");
+  }
+  jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      res.status(401).send("UnAuthorized Access");
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.h0arnkr.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,8 +47,17 @@ async function run() {
     await client.connect();
     const userCollection = client.db("rhythmicDB").collection("users");
     const classCollection = client.db("rhythmicDB").collection("classes");
+
+    // JWT
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_ACCESS_TOKEN, {
+        expiresIn: "2h",
+      });
+      res.send({ token });
+    });
     // users collection
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
@@ -41,6 +71,7 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    
     app.patch("/users/roleUpdate", async (req, res) => {
       const id = req.query.id;
       const role = req.query.role;
@@ -70,25 +101,29 @@ async function run() {
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          status: status
-        }
-      }
+          status: status,
+        },
+      };
       const result = await classCollection.updateOne(filter, updatedDoc);
-      res.send(result)
+      res.send(result);
     });
-    app.patch('/classes/feedback', async (req, res) => {
+    app.patch("/classes/feedback", async (req, res) => {
       const feedback = req.body;
       const id = req.query.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          feedback: feedback
-        }
+          feedback: feedback,
+        },
       };
-      const options ={upsert: true}
-      const result = await classCollection.updateOne(filter, updatedDoc, options);
+      const options = { upsert: true };
+      const result = await classCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
       res.send(result);
-    })
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
